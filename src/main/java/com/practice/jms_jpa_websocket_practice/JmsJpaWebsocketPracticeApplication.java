e39d2e7fa69967d9cde98416faab912ef3782103
OluC94 <coker.olu94@gmail.com>
2024-09-10 14:31:43 +0100

    initial commit

---
+++ src/main/java/com/practice/jms_jpa_websocket_practice/JmsJpaWebsocketPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.practice.jms_jpa_websocket_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsJpaWebsocketPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmsJpaWebsocketPracticeApplication.class, args);
+	}
+
+}
