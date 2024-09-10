e6028aa4847d670f63c2df23d8b1f7a216317c77
OluC94 <coker.olu94@gmail.com>
2024-09-10 14:37:45 +0100

    create customer entity

---
+++ src/main/java/com/practice/jms_jpa_websocket_practice/entities/Customer.java
@@ -0,0 +1,26 @@
+package com.practice.jms_jpa_websocket_practice.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@ToString
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String firstName;
+    private String lastName;
+
+    protected Customer() {}
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
